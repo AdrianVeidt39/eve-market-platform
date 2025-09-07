09471e4eada708bd171a393ddbb540340ee23a9a
AdrianVeidt39 <138620680+AdrianVeidt39@users.noreply.github.com>
2025-09-06 20:39:13 -0600

    Allow cross-origin requests

--- server/app.py
+++ server/app.py
@@ -2,7 +2,15 @@
 from flask import Flask, jsonify, request
 from market_service import MarketService
 
+
+def _add_cors_headers(response):
+    """Allow cross-origin requests so the API works from other ports."""
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+    return response
+
 app = Flask(__name__, static_folder="../client", static_url_path="")
+app.after_request(_add_cors_headers)
 service = MarketService()
 
 
